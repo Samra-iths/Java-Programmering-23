9f35ec7296a1eb8c531d22ad8119e925ac436b8f
samra-iths <samra.assamnew.andargie@iths.se>
2023-09-06 10:28:04 +0200

    Java print codealongs practice

--- codealongs/Print/finished/LearningPrint.java
+++ codealongs/Print/finished/LearningPrint.java
@@ -2,4 +2,22 @@ package codealongs.Print.finished;
 
 public class LearningPrint {
     
+public static void main (String[] args ){
+    System.out.println("Hello World");
+    System.out.println(45);
+    System.out.println('\u00E4');
+
+    int cars = 8;
+
+    System.out.println(cars);
+    Parrot parrot= new Parrot();
+    System.out.println(parrot.name);
+    System.out.print("Hello World");
+
+    System.out.printf("Hello World");
+    
+}
+}
+class Parrot{
+    String name = "Gosta";
 }
